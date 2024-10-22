b733f67597f8d376a514801d22c8100604a0b31d
myvega-2k <myvega2k@gmail.com>
2024-10-22 09:21:44 +0900

    EmployeeServiceImpl update

--- src/main/java/com/restapi/emp/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/restapi/emp/service/impl/EmployeeServiceImpl.java
@@ -68,6 +68,8 @@ public class EmployeeServiceImpl implements EmployeeService {
                 //.collect(Collectors.toList());
     }
 
+    //Employee 정보와 Department 정보를 같이 조회하는 메서드
+    @Transactional(readOnly = true)
     public List<EmployeeDto> getAllEmployeesDepartment() {
         return employeeRepository.findAll()
                 .stream()
@@ -84,6 +86,7 @@ public class EmployeeServiceImpl implements EmployeeService {
                                 HttpStatus.NOT_FOUND)
         );
 
+        //Employee 정보를 수정
         employee.setFirstName(updatedEmployee.getFirstName());
         employee.setLastName(updatedEmployee.getLastName());
         employee.setEmail(updatedEmployee.getEmail());
@@ -94,17 +97,16 @@ public class EmployeeServiceImpl implements EmployeeService {
                                 "Department is not exists with id: " + updatedEmployee.getDepartmentId(),
                                 HttpStatus.NOT_FOUND
                                 ));
-
+        //Employee가 참조하는 Department 정보도 수정
         employee.setDepartment(department);
 
-        Employee updatedEmployeeObj = employeeRepository.save(employee);
+        //Employee updatedEmployeeObj = employeeRepository.save(employee);
 
-        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 
     @Override
     public void deleteEmployee(Long employeeId) {
-
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException(
                         "Employee is not exists with given id: " + employeeId,
