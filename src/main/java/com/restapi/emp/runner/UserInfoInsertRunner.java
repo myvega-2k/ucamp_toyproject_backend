4da618598027c5c874cb696ed6dcb756182adbbf
myvega-2k <myvega2k@gmail.com>
2024-10-31 13:28:02 +0900

    UserInfoInsertRunner add

---
+++ src/main/java/com/restapi/emp/runner/UserInfoInsertRunner.java
@@ -0,0 +1,39 @@
+package com.restapi.emp.runner;
+
+import com.restapi.emp.security.model.UserInfo;
+import com.restapi.emp.security.model.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserInfoInsertRunner implements ApplicationRunner {
+    @Autowired
+    UserInfoRepository userInfoRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setName("adminboot");
+        userInfo.setPassword(
+                passwordEncoder.encode("pwd1"));
+        userInfo.setEmail("admin@aa.com");
+        userInfo.setRoles("ROLE_ADMIN,ROLE_USER");
+
+        UserInfo userInfo2 = new UserInfo();
+        userInfo2.setName("userboot");
+        userInfo2.setPassword(
+                passwordEncoder.encode("pwd2"));
+        userInfo2.setEmail("user@aa.com");
+        userInfo2.setRoles("ROLE_USER");
+
+        userInfoRepository.saveAll(List.of(userInfo, userInfo2));
+
+    }
+}
