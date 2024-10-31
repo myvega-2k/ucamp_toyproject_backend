7bc92113fda6ab53b331e0405460477260694642
myvega-2k <myvega2k@gmail.com>
2024-10-31 15:44:28 +0900

    UserInfoController login add

--- src/main/java/com/restapi/emp/security/controller/UserInfoController.java
+++ src/main/java/com/restapi/emp/security/controller/UserInfoController.java
@@ -1,8 +1,14 @@
 package com.restapi.emp.security.controller;
 
+import com.restapi.emp.security.controller.dto.AuthRequest;
+import com.restapi.emp.security.jwt.JwtService;
 import com.restapi.emp.security.model.UserInfo;
 import com.restapi.emp.security.service.UserInfoUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserInfoController {
 	@Autowired
 	private UserInfoUserDetailsService service;
-	
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtService jwtService;
+
+	@PostMapping("/login")
+	public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(
+						authRequest.getEmail(),
+						authRequest.getPassword()
+				));
+		if (authentication.isAuthenticated()) {
+			return jwtService.generateToken(authRequest.getEmail());
+		} else {
+			throw new UsernameNotFoundException("invalid user request !");
+		}
+	}
+
 	@PostMapping("/new")
 	public String addNewUser(@RequestBody UserInfo userInfo){
 		return service.addUser(userInfo);
