a436d61ecc0ec95687fcf9e3f2aa76b93d4628c1
myvega-2k <myvega2k@gmail.com>
2024-11-01 13:17:13 +0900

    CustomAuthenticationEntryPoint add

---
+++ src/main/java/com/restapi/emp/security/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,41 @@
+package com.restapi.emp.security.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.restapi.emp.exception.advice.ErrorObject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+/*
+    401 인증 실패를 처리하는 클래스
+ */
+@Slf4j
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException, ServletException {
+
+        response.setContentType("application/json;charset=UTF-8");
+        //Set response status code 401
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        ObjectMapper mapper = new ObjectMapper();
+        // Create response content
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpServletResponse.SC_UNAUTHORIZED);
+        errorObject.setMessage("요청된 리소스에 대한 유효한 인증 자격 증명이 실패 하였습니다!!");
+
+        String json = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(errorObject);
+        response.getWriter().write(json);
+
+    }
+}
